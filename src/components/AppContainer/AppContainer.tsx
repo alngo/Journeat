47b97e7e47c96a1ee6e82370f1c322a19007e3b2
Alngo <alngo@student.42.fr>
2020-05-02 20:13:10 +0200

    feat(Container): add container.

---
+++ src/components/AppContainer/AppContainer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+const AppContainer = () => {
+
+    return (
+        <Container maxWidth="xl">
+        <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper >xs=12</Paper>
+        </Grid>
+      </Grid>
+        </Container>
+    )
+
+}
+
+export default AppContainer;
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import AppBar from "components/AppBar/AppBar";
-import AppDrawer from "components/AppDrawer/AppDrawer";
+import AppInterface from "components/AppInterface/AppInterface";
+import AppContainer from "components/AppContainer/AppContainer";
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from "styles/MuiTheme";
-
-const AppInterface = () => {
-  const [isOpen, setOpen] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <AppBar isOpen={isOpen} setOpen={setOpen}/>
-      <AppDrawer isOpen={isOpen} setOpen={setOpen}/>
-    </React.Fragment>
-  )
-}
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <AppInterface />
+      <AppContainer />
     </ThemeProvider>
   );
 }
