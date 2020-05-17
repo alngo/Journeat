26bd6f17bc64480016df13aca425778b5e27b2f3
Alngo <alngo@student.42.fr>
2020-05-17 17:10:12 +0200

    feat(database): prettier.

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Modal from "components/Modal/Modal";
 
 const Onboarding = () => {
-	return (
-		<Modal isOpen={true} handleClose={() => console.log('coucou')}>
-			<div>coucou</div>
-		</Modal>
-	)
-}
+  return (
+    <Modal isOpen={true} handleClose={() => console.log("coucou")}>
+      <div>coucou</div>
+    </Modal>
+  );
+};
 
 export default Onboarding;
