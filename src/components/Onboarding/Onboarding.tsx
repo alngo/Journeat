01f9fe943cfce2742d97789f59c1b843c2bcf1ee
Alngo <alngo@student.42.fr>
2020-05-23 23:26:50 +0200

    feat(onboarding): save profile.

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Modal from "components/Modal/Modal";
+import FormFactory from "components/FormFactory/FormFactory";
+import Button from "@material-ui/core/Button";
+import AppContext from "contexts/AppContext";
 
 type Props = {
   isOpen: boolean;
@@ -7,10 +10,97 @@ type Props = {
 };
 
 const Onboarding = (props: Props) => {
-  const { isOpen } = props;
+  const { state } = React.useContext(AppContext);
+  const profile = state.profile;
+  const [firstname, setFirstame] = React.useState<string>();
+  const [lastname, setLastname] = React.useState<string>();
+  const [gender, setGender] = React.useState<string>();
+  const [age, setAge] = React.useState<string>();
+  const [height, setHeight] = React.useState<string>();
+  const [weight, setWeight] = React.useState<string>();
+
+  const fields = [
+    {
+      id: "Firstname",
+      label: "Firstname",
+      type: "text",
+      onChange: (e: any) => {
+        setFirstame(e.target.value);
+      },
+    },
+    {
+      id: "Lastname",
+      label: "Lastname",
+      type: "text",
+      onChange: (e: any) => {
+        setLastname(e.target.value);
+      },
+    },
+    {
+      id: "Gender",
+      label: "Gender",
+      type: "text",
+      onChange: (e: any) => {
+        setGender(e.target.value);
+      },
+    },
+    {
+      id: "age",
+      label: "Age",
+      type: "number",
+      onChange: (e: any) => {
+        setAge(e.target.value);
+      },
+    },
+    {
+      id: "height",
+      label: "Height",
+      type: "number",
+      onChange: (e: any) => {
+        setHeight(e.target.value);
+      },
+    },
+    {
+      id: "weight",
+      label: "Weight",
+      type: "number",
+      onChange: (e: any) => {
+        setWeight(e.target.value);
+      },
+    },
+  ];
+
+  const handleSubmit = () => {
+    if (firstname && lastname && age && gender && height && weight) {
+      profile.create({
+        firstname: firstname,
+        lastname: lastname,
+        gender: gender,
+        age: Number(age),
+        height: Number(height),
+      });
+      profile.addWeight(Number(weight));
+      props.handleClose();
+    } else console.log("not ok");
+    return;
+  };
+
   return (
-    <Modal isOpen={isOpen} handleClose={() => {}}>
-      <div>coucou</div>
+    <Modal isOpen={true} handleClose={() => {}}>
+      <FormFactory
+        legend="Oboarding"
+        fields={fields}
+        submit={
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+          >
+            Valider
+          </Button>
+        }
+      />
     </Modal>
   );
 };
--- src/database/classes/Profile.tsx
+++ src/database/classes/Profile.tsx
@@ -2,27 +2,50 @@ import { IProfile } from "database/interfaces";
 import Database from "database/Database";
 
 class Profile implements IProfile {
-  id?: number;
+  id!: number;
   db: Database;
   firstname!: string;
   lastname!: string;
+  gender!: string;
   age!: number;
   height!: number;
 
   constructor(db: Database, id?: number) {
     this.db = db;
-    this.id = id || 0;
+    this.id = id || 1;
   }
 
   initialize_profile = async () => {
-    const profile = await this.db.profiles.get(0);
+    const profile = await this.db.profiles.get(this.id);
     if (profile) {
       this.firstname = profile.firstname;
       this.lastname = profile.lastname;
+      this.gender = profile.gender;
       this.age = profile.age;
       this.height = profile.height;
     }
     return profile;
+  };
+
+  create = async (item: IProfile) => {
+    const id = await this.db.profiles.add(item, this.id);
+    this.id = id;
+    this.firstname = item.firstname;
+    this.lastname = item.lastname;
+    this.gender = item.gender;
+    this.age = item.age;
+    this.height = item.height;
+    return id;
+  };
+
+  addWeight = async (weight: number) => {
+    if (this.id) {
+      await this.db.weights.add({
+        profileId: this.id,
+        timestamp: Date.now(),
+        value: weight,
+      });
+    }
   };
 }
 
